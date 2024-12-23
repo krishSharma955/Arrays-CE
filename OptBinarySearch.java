0ac45b0bac3b623a994ab5557a500edcb7089836
Krish Sharma <krishsharma9005@gmail.com>
2024-12-24 00:20:29 +0530

    Optimized Binary Search

---
+++ OptBinarySearch.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class OptBinarySearch { //firtst occurence of an element
+    public static int bSearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length-1;
+        int index = -1;
+        while (start<=end) {
+            int mid = start + (end-start)/2;
+            if(arr[mid]==target) {
+                index = mid;
+                end = mid-1;
+            }
+            else if(arr[mid]>target) {
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,3,3,3,4};
+        int target = 3;
+        System.out.println(bSearch(arr,target));
+    }
+}
